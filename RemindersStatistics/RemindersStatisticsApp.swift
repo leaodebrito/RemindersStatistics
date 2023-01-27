aed17931a0dab2f23349a93899576a9e48b34d99
Bruno Leão <leaodebrito@icloud.com>
2023-01-27 15:47:41 -0300

    first commit

---
+++ RemindersStatistics/RemindersStatisticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersStatisticsApp.swift
+//  RemindersStatistics
+//
+//  Created by Bruno Brito on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct RemindersStatisticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
